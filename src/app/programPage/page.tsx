1ace5bab50217bc1f54080fccdf42b50c286cec6
RafyAM <rafyattala@Rafys-MacBook-Pro.local>
2025-03-16 23:29:23 +0700

    Remove unused Header component and clean up imports; update news section text for consistency

--- src/app/programPage/page.tsx
+++ src/app/programPage/page.tsx
@@ -1,12 +1,11 @@
-import { NewsSection } from "@/components/newsSection";
 import TVProgramPage from "@/components/TVProgramPage";
 
 export default function ProgramPage() {
-    return (
-        <>
-        <div>
-            <TVProgramPage />
-        </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <TVProgramPage />
+      </div>
+    </>
+  );
 }
