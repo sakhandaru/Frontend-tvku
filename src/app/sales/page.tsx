2bc21cab607d7cac4bd3ea9231c7d2d97dd824a1
sakhandaru <rifqiagha7@gmail.com>
2025-03-27 00:20:27 +0700

    Sales

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -1,11 +1,12 @@
 
+import SalesHome from '@/components/salesHome';
 import React from 'react';
 export default function Sales() {
   
   return (
     <>
        <div className='pt-100'>
-        <h1>q</h1>
+        <SalesHome />
        </div>
     </>
   );
