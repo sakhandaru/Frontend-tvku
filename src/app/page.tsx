3e80c603c1244ef03b8acb23459836ea7fe17804
rafyAM <111202214133@mhs.dinus.ac.id>
2025-04-09 11:42:59 +0700

    refactor: adjust layout and spacing in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,43 +29,49 @@ const newsdatas: Inewsdata[] = jsonData.data;
 
 export default function Home() {
   return (
-    <div className="bg-gray-200 mx-3">
+    <div className="bg-gray-200 ">
       <div className="z-0">
         <Carousel />
       </div>
-      <Schedule />
-      <div className="container mx-auto">
-        <h1 className="text-2xl md:text-4xl font-bold mb-6">Berita Terkini</h1>
-        <NewsHeader
-          cover={newsdatas[0].cover}
-          judul={newsdatas[0].judul}
-          deskripsi={newsdatas[0].deskripsi}
-          waktu={newsdatas[0].waktu}
-          kategori={newsdatas[0].kategori}
-        />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {newsdatas.slice(1, 4).map((news, index) => (
-            <NewsCard
-              key={index}
-              cover={news.cover}
-              judul={news.judul}
-              deskripsi={news.deskripsi}
-              waktu={news.waktu}
-              kategori={news.kategori}
-            />
-          ))}
+      <div className="mx-3">
+        <Schedule />
+        <div className="container mx-auto">
+          <h1 className="text-2xl md:text-4xl font-bold mb-6">
+            Berita Terkini
+          </h1>
+          <NewsHeader
+            cover={newsdatas[0].cover}
+            judul={newsdatas[0].judul}
+            deskripsi={newsdatas[0].deskripsi}
+            waktu={newsdatas[0].waktu}
+            kategori={newsdatas[0].kategori}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {newsdatas.slice(1, 4).map((news, index) => (
+              <NewsCard
+                key={index}
+                cover={news.cover}
+                judul={news.judul}
+                deskripsi={news.deskripsi}
+                waktu={news.waktu}
+                kategori={news.kategori}
+              />
+            ))}
+          </div>
         </div>
+        <Program />
       </div>
-      <Program />
-      <div className="bg-white py-20 ">
-        <div>
-          <CarouselCompanyLogo />
-        </div>
-        <div>
-          <About />
-        </div>
-        <div>
-          <OurExpertise />
+      <div >
+        <div className="bg-white py-20 px-3">
+          <div>
+            <CarouselCompanyLogo />
+          </div>
+          <div>
+            <About />
+          </div>
+          <div>
+            <OurExpertise />
+          </div>
         </div>
       </div>
     </div>
