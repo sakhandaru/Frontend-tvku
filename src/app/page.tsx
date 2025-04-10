f6f68b636f664584d4f8bdadc0d298bcccc252bb
rafyAM <111202214133@mhs.dinus.ac.id>
2025-04-10 10:39:44 +0700

    refactor: update background color in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ const newsdatas: Inewsdata[] = jsonData.data;
 
 export default function Home() {
   return (
-    <div className="bg-gray-200 ">
+    <div className="bg-gray-100 ">
       <div className="z-0">
         <Carousel />
       </div>
