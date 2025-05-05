91d79269ed71125ee6035bc96207908290d23325
rafyAM <111202214133@mhs.dinus.ac.id>
2025-05-06 00:14:49 +0700

    fix base url

--- src/app/seputar_udinus/[id]/page.tsx
+++ src/app/seputar_udinus/[id]/page.tsx
@@ -4,6 +4,8 @@ import NewsGrid from "@/components/seputarUdinus/NewsGrid";
 import axios from "axios";
 import { useParams } from "next/navigation";
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export default function SeputarUdinus() {
     const [title, setTitle] = useState("");
     const {id} = useParams();
@@ -18,7 +20,7 @@ export default function SeputarUdinus() {
 
     const getData = async () => {
         try {
-            const {data} = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/seputar-dinus-slider?per_page=12&id_slides_title=${id}&current_page=${currentPage}`);
+            const {data} = await axios.get(`${BASE_URL}/seputar-dinus-slider?per_page=12&id_slides_title=${id}&current_page=${currentPage}`);
             setSliderData(data.data);
         } catch (error) {
             throw error;
@@ -26,7 +28,7 @@ export default function SeputarUdinus() {
     }
     const getTitle = async () => {
         try {
-            const {data} = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/seputar-dinus-slides-title/${id}`);
+            const {data} = await axios.get(`${BASE_URL}/seputar-dinus-slides-title/${id}`);
             setTitle(data.judul);
         } catch (error) {
             throw error;
